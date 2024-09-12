517a8b98c1e906fd300746d88ac931b38c184a54
andresm1991 <andresmacias.1991@gmail.com>
2024-09-12 10:08:26 -0500

    modificacion en public para proyectos archivos y adquisiciones

--- js/adquisiciones_script.js
+++ js/adquisiciones_script.js
@@ -23,6 +23,90 @@ $(function () {
     });
 
     /**
+     * Eliminar pedido
+     */
+    $(document).on('click', '.eliminar-pedido', function () {
+        var csrf = $('meta[name="csrf-token"]').attr('content');
+        var $this = $(this);
+        var id = $(this).attr('id');
+
+        Swal.fire({
+            title: '¿Esta Seguro?',
+            text: "Una vez se elimina el registro no podrá recuperarlo.",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si, deseo Eliminarlo',
+            cancelButtonText: 'Cancelar'
+        }).then((result) => {
+            if (result.value) {
+                $.ajax({
+                    url: 'eliminar-adquisicion/' + id,
+                    headers: { 'X-CSRF-TOKEN': csrf },
+                    type: 'DELETE',
+                    dataType: 'json',
+                })
+                    .done(function (data) {
+                        if (data.success) {
+                            Toast.fire({
+                                icon: 'success',
+                                title: data.message,
+                            });
+
+                            $("#" + id).remove();
+
+                            if ($('tbody').children().length == 0) {
+                                $('tbody').html('<tr>' +
+                                    '<td colspan = "7" class="text-center text-danger"><strong>No se encontraron datos para mostrar.</strong></td>' +
+                                    '</tr>');
+                            }
+                        } else {
+                            Swal.fire(
+                                'Error!',
+                                data.message,
+                                'error'
+                            )
+                        }
+
+                    })
+                    .fail(function () {
+                        Swal.fire(
+                            'Error Inesperado!',
+                            'No se pudo realizar la acción de eliminado, comuníquese con el administrador del sistema.',
+                            'error'
+                        )
+                    });
+            }
+        });
+    });
+    /**
+     * Buscar Pedido
+     * @param String
+     * return JSON
+    */
+    $('input:text[name=adquisicion_search]').on('keyup', function () {
+        var csrf = $('meta[name="csrf-token"]').attr('content');
+        var $value = $(this).val();
+
+        $.ajax({
+            url: url + '/buscar-pedido',
+            headers: { 'X-CSRF-TOKEN': csrf },
+            type: 'GET',
+            data: { 'buscar': $value },
+            beforeSend: function () {
+            },
+            success: function (data) {
+                $('tbody').html(data);
+            }
+        }).fail(function (jqXHR, textStatus, errorThrown) {
+            var errors = JSON.parse(jqXHR.responseText);
+            console.log(errors)
+        });
+    });
+
+
+    /**
      * Agregar elementos a la tabla de pedidos
      */
     $('#add-producto-adquisicion').on('click', function () {
