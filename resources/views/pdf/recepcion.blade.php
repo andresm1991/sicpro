e9eae66d674f68304f8ad18c766c34a47018a37c
andresm1991 <andresmacias.1991@gmail.com>
2024-09-16 12:44:48 -0500

    se modifica la path de la imgen para orden de recepcion

--- resources/views/pdf/recepcion.blade.php
+++ resources/views/pdf/recepcion.blade.php
@@ -98,7 +98,8 @@
 
 <body>
     <div class="header">
-        <img src="{{ public_path('images/logo_empresa.jpg') }}" alt="Logo">
+        <img src="data:image/png;base64,{{ base64_encode(file_get_contents(public_path('images/logo_empresa.jpg'))) }}"
+            alt="Logo">
         <div class="order-number">
             Orden de Recepción <br>{{ $orden['numero_pedido'] }}
         </div>
