08f04af8816a55fca0a3099c68a53b32a9be1378
People Ecosystems <peopleecosystems@MacBook-Air-de-People.local>
2024-09-17 15:53:03 -0500

    camnios desde mac

---
+++ database/migrations/2024_09_17_160007_create_mano_obra_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('mano_obra', function (Blueprint $table) {
+            $table->id();
+            $table->date('fecha');
+            $table->foreignId('proveedor_id')->references('id')->on('proveedores'); 
+            $table->foreignId('articulo_id')->references('id')->on('articulos'); 
+            $table->enum('jornada', ['COMPLETA', 'MEDIO TIEMPO']);
+            $table->double('valor')->default(0);
+            $table->double('adicional')->default(0);
+            $table->double('descuento')->default(0);
+            $table->string('detalle_adicional')->nullable();
+            $table->string('detalle_descuento')->nullable();
+            $table->string('observacion')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('mano_obra', function (Blueprint $table) {
+            Schema::dropIfExists('mano_obra');
+        });
+    }
+};
