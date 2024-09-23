55f92edafff1998f40fc1d7b9230101c413bb1ae
andresm1991 <andresmacias.1991@gmail.com>
2024-09-22 21:37:26 -0500

    bloqueo contratista porque aun no esta

--- resources/views/adquisiciones/tipo_adquisicion.blade.php
+++ resources/views/adquisiciones/tipo_adquisicion.blade.php
@@ -10,24 +10,55 @@
                 @foreach ($aquisiciones as $item)
                     @if ($item->slug != 'profecionales')
                         <div class="col-md-6 d-flex justify-content-center align-items-center">
-                            <a href="{{ route(
-                                $item->slug == 'contratista'
-                                    ? 'proyecto.adquisiciones.contratista'
-                                    : ($item->slug == 'mano.obra'
-                                        ? 'proyecto.adquisiciones.mano.obra'
-                                        : 'proyecto.adquisiciones.tipo.etapa'),
-                                [
-                                    'tipo' => $tipo,
-                                    'tipo_id' => $tipo_id,
-                                    'proyecto' => $proyecto->id,
-                                    'tipo_adquisicion' => $tipo_adquisicion->id,
-                                    'tipo_etapa' => $item->id,
-                                ],
-                            ) }}"
-                                class="nodo">
-                                <img src="{{ asset(json_decode($item->detalle)->icono) }}" alt="">
-                                <span>{{ $item->descripcion }}</span>
-                            </a>
+                            @switch($item->slug)
+                                @case('contratista')
+                                    {{--   <a href="{{ route('proyecto.adquisiciones.contratista', [
+                                        'tipo' => $tipo,
+                                        'tipo_id' => $tipo_id,
+                                        'proyecto' => $proyecto->id,
+                                        'tipo_adquisicion' => $tipo_adquisicion->id,
+                                        'tipo_etapa' => $item->id,
+                                    ]) }}"
+                                        class="nodo">
+                                        <img src="{{ asset(json_decode($item->detalle)->icono) }}" alt="">
+                                        <span>{{ $item->descripcion }}</span>
+                                    </a>
+                                    --}}
+
+                                    <a href="javascripts:void(0);" class="nodo">
+                                        <img src="{{ asset(json_decode($item->detalle)->icono) }}" alt="">
+                                        <span>{{ $item->descripcion }}</span>
+                                    </a>
+                                @break
+
+                                @case('mano.obra')
+                                    <a href="{{ route('proyecto.adquisiciones.mano.obra', [
+                                        'tipo' => $tipo,
+                                        'tipo_id' => $tipo_id,
+                                        'proyecto' => $proyecto->id,
+                                        'tipo_adquisicion' => $tipo_adquisicion->id,
+                                        'tipo_etapa' => $item->id,
+                                    ]) }}"
+                                        class="nodo">
+                                        <img src="{{ asset(json_decode($item->detalle)->icono) }}" alt="">
+                                        <span>{{ $item->descripcion }}</span>
+                                    </a>
+                                @break
+
+                                @default
+                                    <a href="{{ route('proyecto.adquisiciones.tipo.etapa', [
+                                        'tipo' => $tipo,
+                                        'tipo_id' => $tipo_id,
+                                        'proyecto' => $proyecto->id,
+                                        'tipo_adquisicion' => $tipo_adquisicion->id,
+                                        'tipo_etapa' => $item->id,
+                                    ]) }}"
+                                        class="nodo">
+                                        <img src="{{ asset(json_decode($item->detalle)->icono) }}" alt="">
+                                        <span>{{ $item->descripcion }}</span>
+                                    </a>
+                            @endswitch
+
                         </div>
                     @endif
                 @endforeach
