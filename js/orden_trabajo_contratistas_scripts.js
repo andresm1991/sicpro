1093ff5539027726ccd7508fba2931a181398cf5
People Ecosystems <peopleecosystems@MacBook-Air-de-People.local>
2024-10-03 16:25:32 -0500

    buscardo contratista

--- js/orden_trabajo_contratistas_scripts.js
+++ js/orden_trabajo_contratistas_scripts.js
@@ -209,6 +209,85 @@ $(function(){
         $(this).closest('.edit-item').find('span').show();
     });
 
+    $(document).on('click', '.eliminar-orden-trabajo', function(){
+        var $this = $(this);
+        var id = $(this).attr('id');
+
+        Swal.fire({
+            title: '¿Esta Seguro?',
+            text: "Una vez se elimina el registro no podrá recuperarlo.",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si, deseo Eliminarlo',
+            cancelButtonText: 'Cancelar'
+        }).then((result) => {
+            if (result.value) {
+                $.ajax({
+                    url: '/orden-trabajo/eliminar/' + id,
+                    headers: { 'X-CSRF-TOKEN': csrf },
+                    type: 'DELETE',
+                    dataType: 'json',
+                })
+                    .done(function (data) {
+                        if (data.success) {
+                            Toast.fire({
+                                icon: 'success',
+                                title: data.message,
+                            });
+
+                            $("#" + id).remove();
+
+                            if ($('tbody').children().length == 0) {
+                                $('tbody').html('<tr>' +
+                                    '<td colspan = "8" class="text-center text-danger">No se encontraron datos para mostrar.</td>' +
+                                    '</tr>');
+                            }
+                        } else {
+                            Swal.fire(
+                                'Error!',
+                                data.message,
+                                'error'
+                            )
+                        }
+
+                    })
+                    .fail(function () {
+                        Swal.fire(
+                            'Error Inesperado!',
+                            'No se pudo realizar la acción de eliminado, comuníquese con el administrador del sistema.',
+                            'error'
+                        )
+                    });
+            }
+        });
+    });
+
+    /**
+     * Buscar
+     * @param String
+     * return JSON
+    */
+    $('input:text[name=orden_contratista_search]').on('keyup', function () {
+        var $value = $(this).val();
+
+        $.ajax({
+            url: url+'/orden-trabajo/buscar',
+            headers: { 'X-CSRF-TOKEN': csrf },
+            type: 'GET',
+            data: { 'text': $value },
+            beforeSend: function () {
+            },
+            success: function (data) {
+                $('tbody').html(data);
+            }
+        }).fail(function (jqXHR, textStatus, errorThrown) {
+            var errors = JSON.parse(jqXHR.responseText);
+            console.log(errors)
+        });
+    });
+
     function clearInputs() {
         $('#producto').val(null).trigger('change');
         $('#unidad-medida').val(null).trigger('change');
