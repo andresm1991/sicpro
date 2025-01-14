48c7ceba07139bed87d1697741e2ec5d67524a09
andresm1991 <andresmacias.1991@gmail.com>
2025-01-14 14:34:50 -0500

    modulo prestamos

--- js/prestamos_scripts.js
+++ js/prestamos_scripts.js
@@ -3,6 +3,7 @@ import { getFormData } from './helpers.js';
 $(function () {
     var csrf = $('meta[name="csrf-token"]').attr('content');
     var reloadPage = false;
+    var dataEdit = '';
 
     $('#modalPrestamo').on('shown.bs.modal', function (e) {
         $('.select2-basic-single').select2({
@@ -26,11 +27,20 @@ $(function () {
                 $('#proveedores').empty().append('<option></option>');
                 $('#estados').empty().append('<option></option>');
                 $.each(response.proveedores, function (key, value) {
-                    $('#proveedores').append(`<option value="${key}">${value}</option>`);
+                    if (dataEdit != '') {
+                        $('#proveedores').append(`<option value="${key}" ${key == dataEdit.data('trabajador') ? 'selected' : ''}>${value}</option>`);
+                    } else {
+                        $('#proveedores').append(`<option value="${key}">${value}</option>`);
+                    }
                 });
 
                 $.each(response.estados, function (key, value) {
-                    $('#estados').append(`<option value="${key}">${value}</option>`);
+                    if (dataEdit != '') {
+                        $('#estados').append(`<option value="${key}" ${key == dataEdit.data('estado') ? 'selected' : ''}>${value}</option>`);
+                    } else {
+                        $('#estados').append(`<option value="${key}">${value}</option>`);
+                    }
+
                 });
                 $('[data-toggle="tooltip"]').tooltip();
                 $('[data-toggle="popover"]').popover({ html: true });
@@ -43,13 +53,18 @@ $(function () {
             var errors = JSON.parse(jqXHR.responseText);
             console.log(errors)
         });
+
+
+
     });
 
     $("#guardar").on('click', function () {
         var form = $("#form_prestamo");
         var data = getFormData(form);
-
+        var endPoint = 'nuevo';
+        var type = 'POST';
         var valid = true;
+
 
         $('select[name=proveedor], select[name=estado], input[name=monto], input[name=plazo], input[name=interes]').removeClass('error-border');
         $('.select2-basic-single').removeClass('error-border');  // Remover borde rojo en select2
@@ -81,10 +96,15 @@ $(function () {
 
         if (!valid) return;
 
+        if (dataEdit != '') {
+            endPoint = 'actualizar/' + dataEdit.data('prestamo');
+            type = 'PUT';
+        }
+
         $.ajax({
-            url: 'prestamos/nuevo',
+            url: 'prestamos/' + endPoint,
             headers: { 'X-CSRF-TOKEN': csrf },
-            type: 'POST',
+            type: type,
             data: data,
             beforeSend: function () {
 
@@ -112,12 +132,35 @@ $(function () {
         });
     });
 
+    $(document).on('click', '.editar', function () {
+        dataEdit = $(this);
+
+        $('#titleModal').text('Editar Prestamo');
+        $('input:text[name=monto]').val(dataEdit.data('monto'));
+        $('input:text[name=plazo]').val(dataEdit.data('plazo'));
+        $('input:text[name=interes]').val(dataEdit.data('interes'));
+        $('textarea[name=motivo]').val(dataEdit.data('motivo'));
+
+        $('#modalPrestamo').modal({
+            backdrop: 'static', // No permite cerrar el modal al hacer clic fuera
+            keyboard: false     // No permite cerrar el modal usando la tecla ESC
+        }).modal('show');
+    });
+
     // Capturar el clic en "Registrar Pago"
     $(document).on('click', '.registrar-pago', function () {
 
+        let estadoPago = $(this).data('estado');
         let montoProgramado = $(this).data('monto-programado');
         let pago_id = $(this).data('pago');
 
+        if (estadoPago == 'Pagado') {
+            Toast.fire({
+                icon: 'info',
+                title: 'No es posible realizar pago sobre uno ya realizado.'
+            });
+            return;
+        }
         $('input:text[name=monto_programado]').val(montoProgramado);
         $('input:hidden[name=pago_id]').val(pago_id);
         $.ajax({
@@ -147,13 +190,55 @@ $(function () {
             console.log(errors)
         });
 
-        $('#modalPago').modal('show');
+        $('#modalPago').modal({
+            backdrop: 'static', // No permite cerrar el modal al hacer clic fuera
+            keyboard: false     // No permite cerrar el modal usando la tecla ESC
+        }).modal('show');        // Mostrar el modal
+
     });
 
     // Capturar el clic en "Posponer Pago" (opcional)
     $(document).on('click', '.posponer-pago', function () {
-        $('[data-toggle="popover"]').popover('hide');
-        alert('Pago pospuesto');
+        var $this = $(this);
+        var id = $this.data('pago');
+        var $fila = $this.closest('tr');
+
+        Swal.fire({
+            title: '¿Esta Seguro?',
+            text: "Esta opción cambia el estado de pago y no se tomará en cuenta para cálculos futuros.",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si, deseo continuar',
+            cancelButtonText: 'Cancelar'
+        }).then((result) => {
+            if (result.value) {
+                $.ajax({
+                    url: '/administrativo/prestamos/posponer-pago',
+                    headers: { 'X-CSRF-TOKEN': csrf },
+                    type: 'POST',
+                    data: { 'pago': id },
+                    dataType: 'json',
+                })
+                    .done(function (response) {
+                        if (response.success) {
+                            $fila.find('td:nth-child(5)').html(`<span class="badge badge-danger">${response.nuevoEstado}</span>`);
+                        }
+                        Toast.fire({
+                            icon: response.success ? 'success' : 'error',
+                            title: response.mensaje
+                        });
+                    })
+                    .fail(function () {
+                        Swal.fire(
+                            'Error Inesperado!',
+                            'No se pudo realizar la acción, comuníquese con el administrador del sistema.',
+                            'error'
+                        )
+                    });
+            }
+        });
     });
 
     $(document).on('click', '#registrar_pago', function () {
@@ -189,6 +274,7 @@ $(function () {
                 reloadPage = false;
             },
             success: function (response) {
+
                 if (response.success) {
                     reloadPage = true;
 
@@ -216,12 +302,90 @@ $(function () {
         if (reloadPage) {
             location.reload(); // Recarga la página
         }
-        $('#message').hide();
+        $('#message').html('');
         $('.input_errors').remove();
         $('select[name=forma_pago], input[name=monto_programado], input[name=monto_pagado]').removeClass('error-border');
         $('.select2-basic-single').removeClass('error-border');  // Remover borde rojo en select2
         $('.error-message').remove();
 
+    });
+
+
+    $('#modalPrestamo').on('hidden.bs.modal', function (e) {
+        if (reloadPage) {
+            location.reload(); // Recarga la página
+        }
+        dataEdit = '';
+        $('#titleModal').text('Nuevo Prestamo');
+        $('#message').html('');
+        $('.input_errors').remove();
+        $('select[name=proveedor], select[name=estado], input[name=monto], input[name=plazo]').removeClass('error-border');
+        $('.select2-basic-single').removeClass('error-border');  // Remover borde rojo en select2
+        $('.error-message').remove();
+
+    });
+
+    $('.recalcular-pagos-prestamo').on('click', function () {
+        var id = $(this).attr('id');
+        Swal.fire({
+            title: '¿Esta Seguro?',
+            text: "Esta opción dividirá el saldo del préstamo en los pagos pendientes.",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si, deseo continuar',
+            cancelButtonText: 'Cancelar'
+        }).then((result) => {
+            if (result.value) {
+                $.ajax({
+                    url: '/administrativo/prestamos/recalcular_pagos',
+                    headers: { 'X-CSRF-TOKEN': csrf },
+                    type: 'POST',
+                    data: { 'prestamo': id },
+                    dataType: 'json',
+                })
+                    .done(function (response) {
+                        $('tbody').html(response.listPagos);
+                        Toast.fire({
+                            icon: response.success ? 'success' : 'error',
+                            title: response.mensaje
+                        });
+                    })
+                    .fail(function () {
+                        Swal.fire(
+                            'Error Inesperado!',
+                            'No se pudo realizar la acción, comuníquese con el administrador del sistema.',
+                            'error'
+                        )
+                    });
+            }
+        });
+    });
+
+
+    /**
+     * Buscar
+     * @param String
+     * return JSON
+    */
+    $('input:text[name=search_prestamo]').on('keyup', function () {
+        var $value = $(this).val();
+
+        $.ajax({
+            url: '/administrativo/prestamos/buscar-prestamo',
+            headers: { 'X-CSRF-TOKEN': csrf },
+            type: 'GET',
+            data: { 'text': $value },
+            beforeSend: function () {
+            },
+            success: function (data) {
+                $('tbody').html(data);
+            }
+        }).fail(function (jqXHR, textStatus, errorThrown) {
+            var errors = JSON.parse(jqXHR.responseText);
+            console.log(errors)
+        });
     });
 
 });
