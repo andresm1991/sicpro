c506bd639b2c6f3aa753bcbe164e2bb04aa92963
andresm1991 <andresmacias.1991@gmail.com>
2025-01-14 23:08:05 -0500

    nuevos cambios

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+self.addEventListener("push", (event) => {
+    const notification = event.data.json();
+    event.waitUntil(
+        self.registration.showNotification(notification.title, {
+            body: notification.body,
+            icon: "./images/icon_app.png",
+            data: {
+                url: notification.url
+            }
+        })
+    )
+});
+
+self.addEventListener("notificationclick", (event) => {
+    event.waitUntil(
+        clients.openWindow(event.notification.data.url)
+    )
+});
