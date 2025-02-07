3362fc7be903d91d0561ec3c6edfd04e35d1172e
Andres Macias <andresmacias.1991@gmail.com>
2025-02-07 15:28:02 -0500

    cronnograma

--- js/cronograma_scripts.js
+++ js/cronograma_scripts.js
@@ -1,10 +1,34 @@
 $(function() {
-
+    var csrf = $('meta[name="csrf-token"]').attr('content');
     let $tdSeleccionado; // Variable global para almacenar el TD clicado
+
+    // Configuración base para Select2
+    const select2TagConfig = {
+        width: '100%',
+        allowClear: false, // Permite limpiar la selección
+        tags: true, // Permite agregar nuevas opciones escribiendo
+        placeholder: function () {
+            return $(this).data('placeholder');
+        },
+        createTag: function (params) {
+            var term = $.trim(params.term);
+            if (term === '') {
+                return null;
+            }
+            return {
+                id: term,
+                text: term,
+                newTag: true
+            };
+        },
+        insertTag: function (data, tag) {
+            data.unshift(tag); // Inserta la nueva opción al principio
+        }
+    };
 
    $(document).on('click', '.click-semana', function () {
     $tdSeleccionado = $(this); // Guardar referencia al TD clicado
-
+    getActividadesCronograma($('.actividades'));
     // Verificar si el TD ya está pintado
     if ($tdSeleccionado.hasClass('pintado')) {
         $('#modal-text').text('¿Desea despintar esta celda?');
@@ -44,15 +68,35 @@ $(function() {
     let day = $(this).data('day'); // Obtiene el día
     let container = $('#' + day + '-fields'); // Encuentra el contenedor de ese día
     
-    // Crear nuevo campo con botón de eliminar
-    let newField = $('<div class="form-group input-group mb-2">' +
-                        '<input type="text" name="' + day + '[]" class="form-control">' +
-                        '<div class="input-group-append">' +
-                            '<button class="btn btn-danger btn-sm remove-field"><i class="fa-solid fa-xmark"></i></button>' +
-                        '</div>' +
-                    '</div>');
+    // Crear un nuevo <select> con botón de eliminar
+    let newField = $('<div>', {
+        class: 'form-group input-group mb-2',
+        html: [
+            $('<select>', {
+                name: day + '[]',
+                class: 'form-control actividades'
+            }),
+            $('<div>', {
+                class: 'input-group-append',
+                html: $('<button>', {
+                    class: 'btn btn-danger btn-sm remove-field',
+                    html: '<i class="fa-solid fa-xmark"></i>'
+                })
+            })
+        ]
+    });
 
     container.append(newField); // Agrega el campo
+    // Obtener el nuevo <select> creado
+    let $newSelect = container.find('.actividades').last();
+
+    // Inicializar Select2 con la misma configuración base
+    $newSelect.select2($.extend(true, {}, select2TagConfig, {
+        dropdownParent: container.closest('.modal') // Para que funcione dentro de modales
+    }));
+
+    // Cargar opciones dinámicas para el nuevo <select>
+    getActividadesCronograma($newSelect);
 });
 
 // Eliminar campo dinámico
@@ -61,26 +105,55 @@ $(document).on('click', '.remove-field', function () {
 });
 
    $(document).on('shown.bs.modal', '.modal', function () {
-    $(this).find('.select2-tag').each(function () {
+    $(this).find('.actividades').each(function () {
         let $select = $(this);
 
+        // Destruir Select2 si ya está inicializado
         if ($select.data('select2')) {
-            $select.select2('destroy'); // Destruir Select2 si ya está inicializado
+            $select.select2('destroy');
         }
 
-        // Obtener la configuración original almacenada en `data()`
-        let originalOptions = $select.data('select2-config') || {};
+        // Inicializar Select2 con la configuración base y dropdownParent
+        $select.select2($.extend(true, {}, select2TagConfig, {
+            dropdownParent: $(this).closest('.modal') // Para que funcione dentro de modales
+        }));
 
-        // Extender las opciones sin perder `createTag` ni `insertTag`
-        let newOptions = $.extend(true, {}, originalOptions, {
-            dropdownParent: $select.closest('.modal')
-        });
-
-        // Guardar la nueva configuración
-        $select.data('select2-config', newOptions);
-
-        // Inicializar Select2 con la configuración fusionada
-        $select.select2(newOptions);
+        // Cargar opciones dinámicas
+        getActividadesCronograma($select);
     });
    });
+
+   var getActividadesCronograma = function($select) {
+    $.ajax({
+        url: '/actividades-cronograma',
+        headers: { 'X-CSRF-TOKEN': csrf },
+        type: 'GET',
+        success: function (response) {
+            // Limpiar el <select>
+            $select.empty();
+
+            // Agregar una opción vacía para el placeholder
+            $select.append(new Option('', '', false, false));
+
+            // Iterar sobre las actividades y crear nuevas opciones
+            $.each(response.actividades, function (index, actividad) {
+                let option = new Option(actividad, index, false, false);
+                $select.append(option);
+            });
+
+            // Desencadenar el evento 'change' para actualizar Select2
+            $select.trigger('change');
+        }
+    }).fail(function (jqXHR, textStatus, errorThrown) {
+        switch (jqXHR.status) {
+            case 419: // ERROR EXPIRATE SESSION
+                window.location = '/';
+                break;
+
+            default:
+                var errors = JSON.parse(jqXHR.responseText);
+                Swal.fire('Error!',errors,'error');
+        }
+    });
+   };
 });
