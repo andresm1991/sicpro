c2aaafb1b12c028c470ff574c587f30827e01f6f
Andres Macias <andresmacias.1991@gmail.com>
2025-03-25 15:56:06 -0500

    reportes

--- js/reportes_scripts.js
+++ js/reportes_scripts.js
@@ -20,4 +20,69 @@ $(function () {
         // Enviar el formulario
         $('#form-reporte-adquisiciones').submit();
     });
+
+    $('select[name=tipo]').on('change', function () {
+        let tipoText = $('select[name=tipo] option:selected').text();
+        $('select[name=proveedor]').empty();
+        $('select[name=producto]').empty();
+        $('select[name=cargo]').empty();
+
+        if (tipoText != '') {
+            tipoText = tipoText.replace(/\s+/g, "_").toLowerCase();
+            $.ajax({
+                url: 'datos-filtro-reporte-adquisiciones',
+                headers: { 'X-CSRF-TOKEN': csrf },
+                type: 'GET',
+                data: { tipo: tipoText },
+                beforeSend: function () {
+
+                },
+                success: function (response) {
+                    console.log(response);
+                    $('select[name=proveedor]').append('<option value=""></option>');
+                    $.each(response.result.proveedores, function (index, proveedor) {
+                        $('select[name=proveedor]').append(
+                            '<option value="' + proveedor.id + '">' + proveedor.razon_social + '</option>'
+                        );
+                    });
+
+                    $('select[name=producto]').append('<option value=""></option>');
+                    $.each(response.result.productos, function (index, value) {
+                        $('select[name=producto]').append('<option value="' + value.id + '">' + value.descripcion + '</option>');
+                    });
+
+                    $('select[name=cargo]').append('<option value=""></option>');
+                    $.each(response.result.cargos, function (index, value) {
+                        $('select[name=cargo]').append('<option value="' + value.id + '">' + value.descripcion + '</option>');
+                    });
+                    //$("#categoria_tarea").val(response.categoria).trigger('change');
+
+                    $('[data-toggle="tooltip"]').tooltip();
+                    $('[data-toggle="popover"]').popover({ html: true });
+                },
+                complete: function () {
+                    $('#comentarioTareaModal #modal-overlay').hide();
+                }
+            }).fail(function (jqXHR, textStatus, errorThrown) {
+                switch (jqXHR.status) {
+                    case 422: // ERROR INPUT VALIDATE
+
+                        break;
+
+                    case 419: // ERROR EXPIRATE SESSION
+                        window.location = '/';
+                        break;
+
+                    default:
+                        var errors = JSON.parse(jqXHR.responseText);
+                        Swal.fire(
+                            'Ups.!',
+                            'Algo salió mal, por favor vuelva a intentarlo.',
+                            'error'
+                        )
+                        console.log(errors)
+                }
+            });
+        }
+    });
 });
