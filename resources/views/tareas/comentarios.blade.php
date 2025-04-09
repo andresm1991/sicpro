04893ce78b705e474330cd76a0fe8b44a08c4433
andresm1991 <andresmacias.1991@gmail.com>
2025-04-08 21:42:21 -0500

    cambios

---
+++ resources/views/tareas/comentarios.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('title', $title_page)
+
+@section('content')
+    @include('partials.header_page')
+    <section class="content" style="padding-bottom: 20px;">
+        <div class="container">
+            <div class="card">
+
+                <div class="card-body ">
+
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
+
+@section('scripts')
+    <script src="{{ asset('js/tareas_scripts.js') }}" type="module"></script>
+@endsection
