e1de1e19b8db9a9bdcfd3573d6ceb3e14ee2d412
andresm1991 <andresmacias.1991@gmail.com>
2025-05-27 21:34:27 -0500

    subri cambios a produccion

--- resources/views/cronograma/index.blade.php
+++ resources/views/cronograma/index.blade.php
@@ -61,7 +61,8 @@
                                 @forelse ($cronograma as $index => $rubro)
                                     <tr id="{{ $index }}">
                                         <td class="aling-middle">{{ $nro }}</td>
-                                        <td class="aling-middle editar-actividad" id="{{ $rubro['rubro_cronograma_id'] }}">
+                                        <td class="aling-middle editar-actividad" id="{{ $rubro['rubro_cronograma_id'] }}"
+                                            style="cursor: pointer;">
                                             {{ $rubro['rubro_cronograma_nombre'] }}
                                         </td>
                                         @for ($i = 1; $i <= $plazo_semanas; $i++)
