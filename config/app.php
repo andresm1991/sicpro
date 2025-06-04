d2ab337c2af87cd68454a995ce1427a81cf0b8d2
andresm1991 <andresmacias.1991@gmail.com>
2025-06-03 22:33:59 -0500

    cambio en config/app

--- config/app.php
+++ config/app.php
@@ -233,7 +233,7 @@ return [
 
     ],
 
-    'version' => '2.1.10',
+    'version' => '2.1.20',
 
     'diasSemana' => ['lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo'],
 ];
