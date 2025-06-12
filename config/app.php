4ca54a9b555a8b203b8e45ab0c38e03349944924
andresm1991 <andresmacias.1991@gmail.com>
2025-06-12 00:32:36 -0500

    camnbios

--- config/app.php
+++ config/app.php
@@ -233,7 +233,7 @@ return [
 
     ],
 
-    'version' => '2.1.32',
+    'version' => '2.1.35',
 
     'diasSemana' => ['lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo'],
 ];
