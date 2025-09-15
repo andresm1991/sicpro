4f18be628d1bfa56ef75ee49ab3784cf4f52dc62
andresm1991 <andresmacias.1991@gmail.com>
2025-09-15 10:31:57 -0500

    correcion items reportes jp

--- resources/views/jp_limpieza/reportes/index.blade.php
+++ resources/views/jp_limpieza/reportes/index.blade.php
@@ -8,14 +8,14 @@
         <div class="container d-flex align-items-center justify-content-center full-height">
             <div class="row">
                 <div class="max-auto">
-                    <a href="{{ route('jp.limpieza.reporte.adquisiciones') }}" class="nodo">
+                    <a href="{{-- route('jp.limpieza.reporte.adquisiciones') --}}" class="nodo">
                         <img src="{{ asset('images/icons/adquisicion.png') }}" alt="">
                         <span>Adquisiciones</span>
                     </a>
                 </div>
 
                 <div class="max-auto">
-                    <a href="{{-- route('jp.limpieza.reporte.caja') --}}" class="nodo">
+                    <a href="{{ route('jp.limpieza.reporte.caja') }}" class="nodo">
                         <img src="{{ asset('images/svg/flujo-de-dinero.svg') }}" alt="">
                         <span>Caja</span>
                     </a>
