d9c4de915304e929379452168be86a259c4474e8
andresm1991 <andresmacias.1991@gmail.com>
2025-09-18 10:27:36 -0500

    caja

--- resources/views/jp_limpieza/reportes/caja.blade.php
+++ resources/views/jp_limpieza/reportes/caja.blade.php
@@ -39,19 +39,20 @@
                             {{ Form::text('fechas', '', ['class' => 'form-control daterange', 'id' => 'fechas', 'placeholder' => 'seleccione rago de fechas']) }}
                         </div>
 
-                        <div class="form-group col-sm-4">
-                            <label for="lbl_ultima-revision" class="col-form-label ">Fecha última revisión </label>
-                            <input type="text" class="form-control" id="ultima-revision"
-                                value="{{ $ultima_revision_caja->fecha_revision_inicio_formatted ?? 'sin revisión' }} - {{ $ultima_revision_caja->fecha_revision_fin_formatted ?? 'sin revisión' }}"
-                                placeholder="sin revisión" disabled>
-                        </div>
+                        @if (auth()->user()->hasRole(['Administrador', 'Gerencial']))
+                            <div class="form-group col-sm-4">
+                                <label for="lbl_ultima-revision" class="col-form-label ">Fecha última revisión </label>
+                                <input type="text" class="form-control" id="ultima-revision"
+                                    value="{{ $ultima_revision_caja->fecha_revision_inicio_formatted ?? 'sin revisión' }} - {{ $ultima_revision_caja->fecha_revision_fin_formatted ?? 'sin revisión' }}"
+                                    placeholder="sin revisión" disabled>
+                            </div>
 
-                        <div class=" d-flex align-items-center col-sm-4 mt-4 p-0">
-                            <button type="button" class="btn btn-dark" id="guardar-revision-caja">
-                                Finalizar Revisión
-                            </button>
-                        </div>
-
+                            <div class=" d-flex align-items-center col-sm-4 mt-4 p-0">
+                                <button type="button" class="btn btn-dark" id="guardar-revision-caja">
+                                    Finalizar Revisión
+                                </button>
+                            </div>
+                        @endif
                     </div>
                     <div id="table-view-reporte"></div>
                 </div> {{-- end div car body --}}
