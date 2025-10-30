054e96e01996ea6ca19b366a3918043e1a463c7c
andresm1991 <andresmacias.1991@gmail.com>
2025-10-30 12:39:35 -0500

    reporte balance prime finalizado con generacion de pdf

--- config/app.php
+++ config/app.php
@@ -233,7 +233,7 @@ return [
 
     ],
 
-    'version' => '2.3.25',
+    'version' => '2.3.30',
 
     'diasSemana' => ['lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo'],
 ];
