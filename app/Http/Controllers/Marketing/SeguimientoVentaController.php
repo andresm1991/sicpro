842f2ec69ab7b1dea006f36bc2e14e5ca1ba6cec
andresm1991 <andresmacias.1991@gmail.com>
2025-11-10 16:30:39 -0500

    logs

--- app/Http/Controllers/Marketing/SeguimientoVentaController.php
+++ app/Http/Controllers/Marketing/SeguimientoVentaController.php
@@ -150,7 +150,8 @@ class SeguimientoVentaController extends Controller
             return response()->json(['success' => true, 'message' => 'Seguimiento de venta creado exitosamente.']);
         } catch (\Exception $e) {
             DB::rollBack();
-            LogService::log('ERROR', 'Seguimiento ventas', ['message' => $e]);
+            return $e;
+            LogService::log('ERROR', 'Seguimiento ventas', ['message' => 'Ocurrió un error al crear el seguimiento de venta: ' . $e->getMessage()]);
             return response()->json(['success' => false, 'message' => 'Ocurrió un error al crear el seguimiento de venta']);
         }
     }
