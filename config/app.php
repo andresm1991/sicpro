fdae89d57c873da925c7352d1608d2aa22de61b9
Andres Macias <andresmacias.1991@gmail.com>
2025-11-21 10:59:28 -0500

    correcion en adquisicones adminsitrativas

--- config/app.php
+++ config/app.php
@@ -233,7 +233,7 @@ return [
 
     ],
 
-    'version' => '2.3.30',
+    'version' => '2.3.34',
 
     'diasSemana' => ['lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo'],
 ];
