2aecbca75939d1d7172585c555762bfe91d58e92
andresm1991 <andresmacias.1991@gmail.com>
2026-01-05 13:46:31 -0500

    correcion reporte comparativo mano de obra

--- config/app.php
+++ config/app.php
@@ -233,7 +233,7 @@ return [
 
     ],
 
-    'version' => '2.3.45',
+    'version' => '2.3.50',
 
     'diasSemana' => ['lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo'],
 ];
