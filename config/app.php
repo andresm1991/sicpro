40234c10c7c9c992cb2bda0106679f79428cfdc3
andresm1991 <andresmacias.1991@gmail.com>
2026-02-09 15:07:00 -0500

    new version app

--- config/app.php
+++ config/app.php
@@ -233,7 +233,7 @@ return [
 
     ],
 
-    'version' => '2.3.51',
+    'version' => '2.3.52',
 
     'diasSemana' => ['lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo'],
 ];
